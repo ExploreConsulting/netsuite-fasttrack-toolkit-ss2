bf90e329439a73605d406167e2fdc3285149a6e3
shawn talbert <ironside14@gmail.com>
2016-04-21 11:34:31 -0700

    fixup refactoring to work in NS

--- EC_Logger.ts
+++ EC_Logger.ts
@@ -3,36 +3,46 @@
  * @NApiVersion 2.x
  */
 
-import {Logger, addAppender, logLevel,getLogger, Appender} from "./aurelia-logging"
+import {Logger, addAppender, logLevel, getLogger, Appender} from "./aurelia-logging"
 import * as nslog from "N/log"
 export {getLogger, Logger, logLevel} from './aurelia-logging'
 
-    
-    /**
-     * Log appender targeting the NS execution log
-     * Severities are mapped as follows:
-     *
-     * debug -> NS 'DEBUG'
-     * info -> NS 'AUDIT'
-     * warn -> NS 'ERROR'
-     * error -> NS 'emergency'
-     */
-    class ExecutionLogAppender implements Appender {
 
-        // invokes the nsdal log function, DRY
-        private log(func,logger:Logger,...rest:any[] ) {
-           var [title, details] = rest
-           var title = logger.id === 'default' ? title : `[${logger.id}] ${title}`
-           return func.bind(nslog,title, details)
-        }
+// invokes the nsdal log function and handles adding a title tag 
+function log(func:string, logger:Logger, ...rest:any[]) {
+   var [title, details] = rest
+   var title = logger.id === 'default' ? title : `[${logger.id}] ${title}`
+   nslog[func](title, details)
+}
 
-        //debug =  _.partial(this.log, 'debug')
-        debug: (logger:Logger, ...rest:any[]) => void = _.partial(this.log, 'debug')
-        info: (logger:Logger, ...rest:any[]) => void = _.partial(this.log, 'info')
-        warn: (logger:Logger, ...rest:any[]) => void = _.partial(this.log, 'warn')
-        error: (logger:Logger, ...rest:any[]) => void = _.partial(this.log, 'error')
+/**
+ * Log appender targeting the NS execution log
+ * Severities are mapped as follows:
+ *
+ * debug -> NS 'DEBUG'
+ * info -> NS 'AUDIT'
+ * warn -> NS 'ERROR'
+ * error -> NS 'emergency'
+ */
+class ExecutionLogAppender implements Appender {
 
-    }
+
+   debug(logger:Logger, ...rest:any[]) {
+      log('debug', logger, ...rest)
+   }
+
+   info(logger:Logger, ...rest:any[]) {
+      log('audit', logger, ...rest)
+   }
+
+   warn(logger:Logger, ...rest:any[]) {
+      log('error', logger, ...rest)
+   }
+
+   error(logger:Logger, ...rest:any[]) {
+      log('emergency', logger, rest)
+   }
+}
 
 // instantiate the default logger and set it's logging level to the most verbose - this is used as
 // the 'main' logger by consumers
