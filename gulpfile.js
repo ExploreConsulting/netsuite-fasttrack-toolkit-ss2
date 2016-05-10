f49d85e91d7659bf99e4d677df4b5aedbd18ca17
stalbert <stalbert@exploreconsulting.com>
2016-05-10 07:36:39 -0700

    create ZIP file of the library suitable for 'advanced' add into NS file cabinet. Zip now includes a folder with a unique name and version stamp so it is possible to have more than one version of the library in use.  update 'del' package because the old version seemed to have race condition errors.

--- gulpfile.js
+++ gulpfile.js
@@ -6,11 +6,10 @@
 // main version stamp - gets appended to the build filename
 var version = require("./package.json").version;
 var outdir = "dist";
-var merge = require('merge-stream')
 var gulp = require('gulp');
 var del = require("del");
 var path = require('path');
-
+var merge = require('merge-stream')
 // loads all gulp-* plugins listed as dependencies from package.json
 var $ = require('gulp-load-plugins')();
 
@@ -22,17 +21,22 @@ var sources = ['EC_Logger.js', 'DataAccess/*.js'];
 var includedNPMlibs = ['node_modules/lodash/lodash.js',
    'node_modules/moment/moment.js', 'node_modules/aurelia-logging/dist/amd/aurelia-logging.js'];
 
-var buildFolderName = "/NFT-SS2-" + version
+var buildFolderName = "/NFT-SS2-" + version;
 
+var versionedDistPath = path.join(outdir,buildFolderName);
+
+// copies files into a staging location in prep for ZIP
 gulp.task('copyfiles', ['clean'], function() {
-   return merge(gulp.src(sources, { base: './'}), gulp.src(includedNPMlibs))
-      .pipe($.copy( path.join(outdir,buildFolderName)))
+   return merge(gulp.src(sources, { base:'.' }), gulp.src(includedNPMlibs))
+      .pipe($.debug({title:'copying core files:'}))
+      .pipe(gulp.dest(versionedDistPath))// 
 })
+
 
 // wrap things up into a single zip file suitable for 'advanced add' extraction to the NetSuite file cabinet.
 gulp.task('default',['copyfiles'], function () {
     // use merge to lift the node dependencies to the root directory of the zip (note no {base:} option)
-    return gulp.src(path.join(outdir,buildFolderName) + '/*/**', { base: './'})
+    return gulp.src(path.join(outdir, '/**'))
         .pipe($.zip(buildFolderName + ".zip")) // combine into a single file w/version stamp
         .pipe(gulp.dest(outdir))
         .pipe($.size()); // outputs a blurb about how many bytes the final result is
@@ -40,7 +44,5 @@ gulp.task('default',['copyfiles'], function () {
 
 // uses plain node del module to delete previous build
 gulp.task('clean', function(){
-    del(outdir, function() {
-        console.log("Deleted " + outdir);
-    });
+    return del(outdir + '/**');
 });
