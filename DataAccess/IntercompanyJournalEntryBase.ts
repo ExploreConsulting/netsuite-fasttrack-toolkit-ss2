62d908784226d764bf87ce1d8f9c3c259ac4e1ae
shawn talbert <ironside14@gmail.com>
2016-06-27 16:43:13 -0700

    * add special Intercompany journal entry line sublist because it has special fields :)

--- DataAccess/IntercompanyJournalEntryBase.ts
+++ DataAccess/IntercompanyJournalEntryBase.ts
@@ -4,6 +4,7 @@
 
 import * as record from 'N/record'
 import {Base as JournalEntry} from "../declarations/DataAccess/JournalEntryBase";
+import {SublistLine,SublistFieldType} from "./Sublist";
 
 /**
  * defines an Inter-company journal entry (basically identical to a normal journal entry?)
@@ -12,3 +13,14 @@ export class Base extends JournalEntry {
    static recordType = record.Type.INTER_COMPANY_JOURNAL_ENTRY
 }
 
+export class LineSublist extends SublistLine {
+
+   @SublistFieldType.select
+   location:number
+
+    /**
+     * the line-level subsidiary - this is a difference between normal journal entry and intercompany JE
+     */
+   @SublistFieldType.select
+   linesubsidiary:number
+}
