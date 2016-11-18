560a9a08289bc369622097ab3d1b0aba6222f4a2
Shawn Talbert <ironside14@gmail.com>
2016-11-18 10:08:13 -0800

    automatically log object properties for log 'details' parameter and switch to 'let' over 'var' as a best practice (and WebStorm now complains about it by default)

--- EC_Logger.ts
+++ EC_Logger.ts
@@ -8,6 +8,7 @@ import {Logger, addAppender, logLevel, getLogger, Appender, LogLevel} from "./au
 import * as nslog from "N/log"
 import * as runtime from "N/runtime"
 import * as aop from "./aop"
+import * as _ from "./lodash"
 
 export {getLogger, Logger, logLevel} from './aurelia-logging'
 
@@ -15,7 +16,7 @@ export {getLogger, Logger, logLevel} from './aurelia-logging'
  * Value to be prepended to each log message title. Defaults to a random 4 digit integer
  * @type {string}
  */
-export var correlationId = Math.floor(Math.random() * 10000).toString();
+export let correlationId = Math.floor(Math.random() * 10000).toString();
 
 /**
  * if true then log message include a random integer (or your custom) prefix to each log entry title.
@@ -23,19 +24,19 @@ export var correlationId = Math.floor(Math.random() * 10000).toString();
  * of the same script (e.g. multiple runs of a scheduled script or discerning between multiple simultaneous calls
  * to a RESTlet or Suitelet)
  */
-export var includeCorrelationId = false;
+export let includeCorrelationId = false;
 
 /**
  * Controls whether the correlation id prefixes should be included in log messages or not.
  * @param enable if true, adds correlationid to the log messages, otherwise no correlation id prefix is added
  */
-export var setIncludeCorrelationId = (enable:boolean) => includeCorrelationId = enable
+export let setIncludeCorrelationId = (enable:boolean) => includeCorrelationId = enable
 
 
 // invokes the nsdal log function and handles adding a title tag 
 function log(loglevel:number, logger:Logger, ...rest:any[]) {
-   var [title, details] = rest
-   var prefix = ''
+   let [title, details] = rest
+   let prefix = ''
 
    if (includeCorrelationId === true) {
       prefix += `${correlationId}>`
@@ -44,7 +45,11 @@ function log(loglevel:number, logger:Logger, ...rest:any[]) {
    if (logger.id !== 'default') {
       prefix += `[${logger.id}]`
    }
-
+   // NetSuite now supports logging js objects but does not log properties from the prototype chain. This is
+   // basically how JSON.stringify() works so I presume they are doing that?
+   // To cover the most common use case of logging an object to see its properties, first convert to
+   // a plain object if it's not one already.
+   if (_.isObject(details) && (!_.isPlainObject(details)) ) details = _.toPlainObject(details)
    nslog[toNetSuiteLogLevel(loglevel)](`${prefix} ${title}`, details)
 }
 
@@ -64,6 +69,11 @@ class ExecutionLogAppender implements Appender {
       log(logLevel.debug, logger, ...rest)
    }
 
+   /**
+    * Info about info
+    * @param logger
+    * @param rest
+    */
    info(logger:Logger, ...rest:any[]) {
       log(logLevel.info, logger, ...rest)
    }
@@ -79,7 +89,7 @@ class ExecutionLogAppender implements Appender {
 
 // instantiate the default logger and set it's logging level to the most verbose - this is used as
 // the 'main' logger by consumers
-var defaultLogger = getLogger('default')
+let defaultLogger = getLogger('default')
 defaultLogger.setLevel(logLevel.debug)
 
 // maps aurelia numeric levels to NS string level names
@@ -121,25 +131,25 @@ export function autoLogMethodEntryExit(methodsToLogEntryExit: {target:Object, me
 
    if (!config) config = {}
    // include method parameters by default
-   var withArgs = config.withArgs !== false
+   let withArgs = config.withArgs !== false
    // include return values by default
-   var withReturnValue = config.withReturnValue !== false
+   let withReturnValue = config.withReturnValue !== false
    // default to not show profiling info
-   var withProfiling = config.withProfiling === true
+   let withProfiling = config.withProfiling === true
    // default to not show governance info
-   var withGovernance = config.withGovernance === true
+   let withGovernance = config.withGovernance === true
    // logger on which to autolog, default to the top level 'Default' logger used by scripts
-   var logger = config.logger || DefaultLogger
+   let logger = config.logger || DefaultLogger
 
    return aop.around(methodsToLogEntryExit, function (invocation) {
       // record function entry with details for every method on our explore object
       log(config.logLevel || logLevel.debug,logger,`Enter ${invocation.method}() ${getGovernanceMessage(withGovernance)}`,
          withArgs ? 'args: ' + JSON.stringify(arguments[0].arguments) : null)
-      var startTime = moment()
-      var retval    = invocation.proceed()
-      var elapsedMessage
+      let startTime = moment()
+      let retval    = invocation.proceed()
+      let elapsedMessage
       if (withProfiling) {
-         var elapsedMilliseconds = moment().diff(startTime);
+         let elapsedMilliseconds = moment().diff(startTime);
          elapsedMessage          = elapsedMilliseconds + "ms = " +
             moment.duration(elapsedMilliseconds).asMinutes().toFixed(2) + " minutes";
       }
@@ -158,8 +168,17 @@ export function autoLogMethodEntryExit(methodsToLogEntryExit: {target:Object, me
  * Configuration options for AutoLogMethodEntryExit
  */
 export interface AutoLogConfig {
+   /**
+    * set true to include automatically include passed method arguments in the logs
+    */
    withArgs?:boolean
+   /**
+    * If true, includes the function return value in the log
+    */
    withReturnValue?:boolean
+   /**
+    *
+    */
    withProfiling?:boolean
    withGovernance?:boolean
    logger?:Logger
@@ -167,15 +186,15 @@ export interface AutoLogConfig {
 }
 
 /**
- * The default logger - this should be the main logger used in scripts
+ * The default logger - this should be the main top level logger used in scripts
  */
-export var DefaultLogger : Logger = defaultLogger
+export let DefaultLogger : Logger = defaultLogger
 
 /**
  * Use to set the correlation id to a value other than the default random number
  * @param value new correlation id, will be used on all subsequent logging
  */
-export var setCorrelationId = (value:string) => correlationId = value
+export let setCorrelationId = (value:string) => correlationId = value
 
 addAppender(new ExecutionLogAppender())
 
