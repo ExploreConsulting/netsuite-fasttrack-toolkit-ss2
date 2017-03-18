9b6b4e6887a61a34d7c14e13d803f4b3e2d5982e
stalbert <stalbert@exploreconsulting.com>
2017-03-17 17:23:36 -0700

    Rudimentary Transfer Order record definition

---
+++ DataAccess/TransferOrderBase.ts
@@ -0,0 +1,113 @@
+/**
+ *  Represents a Transfer Order (transferorder) transaction type in NetSuite
+ */
+
+import {SublistLine, SublistFieldType} from './Sublist'
+import * as record from 'N/record'
+import {TransactionBase} from "./Transaction";
+import {FieldType} from "./Record";
+import * as moment from "../NFT-SS2-0.4.2/moment";
+
+/**
+ * NetSuite Transfer Order Record
+ */
+export class Base extends TransactionBase {
+   static recordType = record.Type.TRANSFER_ORDER
+
+   @FieldType.select
+   "class": number
+
+   @FieldType.select
+   createdfrom: number
+
+   @FieldType.select
+   employee: number
+
+   @FieldType.checkbox
+   firmed: boolean
+
+   @FieldType.select
+   nexus: number
+
+   @FieldType.select
+   orderstatus: number
+
+   @FieldType.checkbox
+   shipcomplete: boolean
+
+   @FieldType.date
+   shipdate: moment.Moment
+
+   @FieldType.currency
+   subtotal: number
+
+   @FieldType.currency
+   total: number
+
+   @FieldType.select
+   transferlocation: number
+
+   @FieldType.checkbox
+   useitemcostastransfercost: boolean
+}
+
+/**
+ * Sublist 'item' on the Tranfer Order record
+ */
+export class ItemSublist extends SublistLine {
+
+   @SublistFieldType.currency
+   amount: number
+
+   @SublistFieldType.select
+   catchupperiod: number
+
+   @SublistFieldType.select
+   commitinventory: number
+
+   @SublistFieldType.checkbox
+   deferrevrec: string
+
+   @SublistFieldType.textarea
+   description: string
+
+   @SublistFieldType.date
+   expectedreceiptdate: string
+
+   @SublistFieldType.date
+   expectedshipdate: string
+
+   @SublistFieldType.checkbox
+   isclosed: boolean
+
+   @SublistFieldType.select
+   item: number
+
+   @SublistFieldType.integernumber
+   linenumber: number
+
+   @SublistFieldType.float
+   quantity: number
+
+   @SublistFieldType.float
+   quantityavailable: number
+
+   @SublistFieldType.float
+   quantitybackordered: number
+
+   @SublistFieldType.float
+   quantitycommitted: number
+
+   @SublistFieldType.float
+   quantityfulfilled: number
+
+   @SublistFieldType.float
+   quantityreceived: number
+
+   @SublistFieldType.currency
+   rate: number
+
+   @SublistFieldType.select
+   units: number
+}
+
