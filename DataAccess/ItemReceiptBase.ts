7db25faf19af6405b4306816b1a5156ef789b6c0
stalbert <stalbert@exploreconsulting.com>
2017-03-17 17:05:26 -0700

    Rudimentary Item Receipt record definition

---
+++ DataAccess/ItemReceiptBase.ts
@@ -0,0 +1,69 @@
+/**
+ * Represents an Item Receipt (itemreceipt) transaction type in NetSuite
+ */
+
+
+import {FieldType} from './Record'
+import * as record from 'N/record'
+import {TransactionBase} from "./Transaction";
+import {SublistLine, SublistFieldType} from './Sublist'
+import * as moment from "../moment"
+
+export class Base extends TransactionBase {
+
+   static recordType = record.Type.ITEM_RECEIPT
+
+   @FieldType.select
+   class:number
+
+   /**
+    * This field shows the purchase order this item receipt is created from.
+    */
+   @FieldType.select
+   createdfrom:number
+
+   @FieldType.select
+   itemfulfillment:number
+
+   @FieldType.checkbox
+   landedcostperline:boolean
+
+   @FieldType.select
+   location:number
+
+}
+/**
+ * Item Receipt Items (item) sublist
+ */
+export class ItemSublist extends SublistLine {
+
+   @SublistFieldType.freeformtext
+   class:string
+
+   @SublistFieldType.freeformtext
+   countryofmanufacture:string
+
+   @SublistFieldType.freeformtext
+   item:string
+
+   @SublistFieldType.checkbox
+   itemreceive:boolean
+
+   @SublistFieldType.select
+   location:number
+
+   @SublistFieldType.float
+   onhand:number
+
+   @SublistFieldType.date
+   revrecenddate:moment.Moment
+
+   @SublistFieldType.decimalnumber
+   quantity:number
+
+   @SublistFieldType.checkbox
+   restock:boolean
+
+   @SublistFieldType.freeformtext
+   units:string
+}
