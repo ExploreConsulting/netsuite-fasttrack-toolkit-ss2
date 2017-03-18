8d4027ebe03632c89cebe14feb7a72f97a22eb47
stalbert <stalbert@exploreconsulting.com>
2017-03-17 17:29:13 -0700

    Rudimentary Intercompany Transfer Order record definition

---
+++ DataAccess/IntercompanyTransferOrderBase.ts
@@ -0,0 +1,28 @@
+/**
+ *  Represents an Intercompany Transfer Order (intercompanytransferorder) transaction type in NetSuite
+ */
+
+import {SublistLine, SublistFieldType} from './Sublist'
+import * as record from 'N/record'
+import * as TransferOrder from "./TransferOrderBase";
+import {FieldType} from "./Record";
+import * as moment from "../NFT-SS2-0.4.2/moment";
+
+/**
+ * NetSuite Intercompany Transfer Order Record
+ * Primary difference between this an a regular Transfer order is a TO destination subsidiary.
+ */
+export class Base extends TransferOrder.Base {
+   static recordType = record.Type.INTER_COMPANY_TRANSFER_ORDER
+
+   @FieldType.select
+   tosubsidiary:number
+}
+
+/**
+ * Sublist 'item' on the Intercompany Tranfer Order record (same as on regular Transfer Order)
+ */
+export class ItemSublist extends TransferOrder.ItemSublist {
+
+}
+
