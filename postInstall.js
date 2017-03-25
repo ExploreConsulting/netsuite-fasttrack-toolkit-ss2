a81e9e2d78566568308a637edfa0d7ddd4164dd0
stalbert <stalbert@exploreconsulting.com>
2017-03-25 11:03:48 -0700

    change postinstall so that it actually copies the declarations rather than symlinks. This makes it possible for a user to have type declarations for more than one version of NFT living a sibling folders (e.g. NFT-SS2-0.4.2/ and NFT-SS2-0.5.1/)

--- postInstall.js
+++ postInstall.js
@@ -1,23 +1,20 @@
 /**
  * Post-install script to satisfy TypeScript install paths in a way that's compatible with NetSuite module resolution.
- * This creates a symbolic link at the project level (assuming typical install path of
+ * This copies the declarations to the  project level (assuming typical install path of
  * node_modules/netsuite-fasttrack-toolkit-ss2). This allows users to upload dist/NFT-SS2-x.y.z.zip in their main
- * suitescript folder and refer to it with relative imports of the form 'import ... from ./NFT-SS2-x.y.z/...'
+ * suitescript folder and refer to it with relative imports of the form 'import ... from ./NFT-SS2-x.y.z/...' and
+ * also support multiple versions of NFT side by side in a single project.
  */
-var fs = require('fs')
+var fs = require('fs-extra')
 var path = require('path')
 
-var target = path.join(process.cwd(),"declarations")
-var path = '../../NFT-SS2-' + process.env.npm_package_version;
-try {
-   if (fs.lstatSync(path) && fs.lstatSync(path).isSymbolicLink()) {
-      console.log('removing existing folder link:' + path)
-      fs.unlinkSync(path)
-   }
-} catch(e) {
-   console.log('existing link not found or could not delete old link - trying to create link as new')
-   // ignoring error on purpose - if we can't delete the link we'll go ahead and try to create it as new
-}
+var source = path.join(process.cwd(),"declarations")
+var destination = '../../NFT-SS2-' + process.env.npm_package_version;
+console.log(`copying NFT declarations from ${source} to ${destination}`)
+fs.copy(source,destination,{ preserveTimestamps:true}, err => {
+      console.error(err)
+})
 
-console.log('creating symbolic link to ' + path )
-fs.symlinkSync(target,path,'dir')
+
+
+
