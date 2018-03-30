31d8b71769a0dd98dc983698368bb5d2af4aff29
stalbert <stalbert@exploreconsulting.com>
2018-03-29 19:49:32 -0700

    enriching the definition for regular Journal Entry so the next slug can benefit.

--- DataAccess/JournalEntryBase.ts
+++ DataAccess/JournalEntryBase.ts
@@ -5,8 +5,11 @@
 import {FieldType, NetsuiteRecord} from './Record'
 import * as record from 'N/record'
 import {SublistLine, SublistFieldType} from './Sublist'
+import * as moment from '../moment'
 
-
+/**
+ * Base class for Journal Entry Record
+ */
 export class Base extends NetsuiteRecord {
    static recordType = record.Type.JOURNAL_ENTRY
 
@@ -17,15 +20,81 @@ export class Base extends NetsuiteRecord {
    approved:boolean
 
    @FieldType.select
+   class:number
+
+   @FieldType.datetime
+   createddate:moment.Moment
+
+   @FieldType.select
+   currency:number
+
+   @FieldType.select
    department:number
+
+   @FieldType.currency
+   exchangerate:number
+
+   @FieldType.freeformtext
+   memo:string
+
+   @FieldType.date
+   reversaldate:moment.Moment
+
+   @FieldType.checkbox
+   reversaldefer:boolean
 
    @FieldType.select
    subsidiary:number
+
+   @SublistFieldType.date
+   trandate:moment.Moment
+
+   @SublistFieldType.freeformtext
+   tranid:string
 }
 
+/**
+ * 'line' sublist on the standard Journal Entry Record
+ */
 export class LineSublist extends SublistLine {
 
    @SublistFieldType.select
+   account:number
+
+   @SublistFieldType.currency
+   credit:number
+
+   @SublistFieldType.currency
+   credittax:number
+
+   @SublistFieldType.currency
+   debit:number
+
+   @SublistFieldType.currency
+   debittax:number
+
+   @SublistFieldType.select
+   department:number
+
+   @SublistFieldType.checkbox
+   eliminate:boolean
+
+   @SublistFieldType.date
+   enddate:moment.Moment
+
+   @SublistFieldType.select
+   entity:number
+
+   @SublistFieldType.select
    location:number
-   
+
+   @SublistFieldType.freeformtext
+   memo:string
+
+   @SublistFieldType.select
+   subsidiary:number
+
+   @SublistFieldType.currency
+   totalamount:number
+
 }
