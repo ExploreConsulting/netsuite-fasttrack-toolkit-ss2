9a910d62495e9b81dd8ad33cef39349cf4736052
stalbert <stalbert@exploreconsulting.com>
2018-04-16 16:17:27 -0700

    add basic AssemblyBuild record

---
+++ DataAccess/AssemblyBuildBase.ts
@@ -0,0 +1,104 @@
+import * as record from 'N/record'
+import {FieldType, NetsuiteRecord} from "NFT/DataAccess/Record"
+import * as moment from "../moment"
+import {SublistLine, Sublist, SublistFieldType} from 'NFT/DataAccess/Sublist'
+
+/**
+ * the Components (component) sublist on AssemblyBuild records
+ */
+export class ComponentSublist extends SublistLine {
+  @SublistFieldType.freeformtext
+  item:string
+
+  @SublistFieldType.integernumber
+  linenumber:number
+
+  @SublistFieldType.float
+  quantity:number
+
+  @SublistFieldType.float
+  quantityonhand:number
+
+  @SublistFieldType.float
+  unitcost:number
+}
+
+/**
+ * NetSuite Assembly Build (assemblybuild) transaction type.
+ * Note it does not inherit from our transaction base because it has a differing subset of fields documented in the
+ * records browser
+ */
+export class Base extends NetsuiteRecord {
+
+  static recordType: record.Type = record.Type.ASSEMBLY_BUILD
+
+  @FieldType.select
+  billofmaterials:number
+
+  @FieldType.select
+  billofmaterialsrevision:number
+
+  @FieldType.float
+  buildable:number
+
+  @FieldType.select
+  class:number
+
+  @FieldType.datetime
+  createddate:moment.Moment
+
+  @FieldType.select
+  customform:number
+
+  @FieldType.select
+  department:number
+
+  @FieldType.freeformtext
+  externalid:string
+
+  /**
+   * Select the assembly item you want to build. You must create assembly item records first before you can build assembly items.
+   */
+  @FieldType.select
+  item:number
+
+  @FieldType.datetime
+  lastmodifieddate:moment.Moment
+
+  @FieldType.select
+  location:number
+
+  @FieldType.freeformtext
+  memo:string
+
+  @FieldType.select
+  postingperiod:number
+
+  /**
+   * Enter the number of assembly items you want to build or unbuild. You cannot build a number higher than the number
+   * in the Buildable Quantity (buildable) field. You cannot unbuild a number higher than the number in the Built field.
+   */
+  @FieldType.float
+  quantity:number
+
+  @FieldType.select
+  revision:number
+
+  @FieldType.textarea
+  revisionmemo:string
+
+  @FieldType.select
+  subsidiary:number
+
+  @FieldType.date
+  trandate:moment.Moment | string
+
+  @FieldType.freeformtext
+  tranid:string
+
+  @FieldType.select
+  units:number
+
+  @FieldType.sublist(ComponentSublist)
+  component: Sublist<ComponentSublist>
+}
