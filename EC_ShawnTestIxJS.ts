8bfafc7e0a70bb5537ddae577a389995daf27fb5
stalbert <stalbert@exploreconsulting.com>
2018-06-26 22:38:01 -0700

    OMG finally have something that works in NS that isn't painful.
    Immutable.js has a lazy sequence implementation. Tried that and it looks promising (after pain with RxJs, IxJs, WUjs and others)

--- EC_ShawnTestIxJS.ts
+++ EC_ShawnTestIxJS.ts
@@ -11,74 +11,82 @@
 
 import * as LogManager from "./NFT-SS2-2.0.1/EC_Logger"
 import * as search from "N/search"
-import * as _ from "./NFT-SS2-2.0.1/lodash"
 import {nsSearchResult2obj} from "./NFT-SS2-2.0.1/search"
+import {Seq} from "immutable"
 
-import * as Ix from "ix"
+
+class IterableSearch {
+
+   static LOGNAME = 'lazy'
+
+   static loadSearch(id: string, pageSize?: number) {
+      return new IterableSearch(search.load({id: id}), pageSize)
+   }
+   log:LogManager.Logger
+   pagedData: search.PagedData
+   currentPage: search.Page
+   currentData: search.Result[]
+   index: number
+
+   // //@ts-ignore
+   // ["@@iterator"](): Iterator<search.Result | null> {
+   //    return this as Iterator<search.Result | null>
+   // }
+
+   private constructor(private search: search.Search, private pageSize?: number) {
+      this.log = LogManager.getLogger(IterableSearch.LOGNAME)
+      this.pagedData = this.search.runPaged({pageSize: pageSize})
+      this.currentPage = this.pagedData.fetch({index: 0})
+      this.currentData = this.currentPage.data
+      this.index = 0
+      this.log.info(`lazy search ${search.searchId}`, `using page size ${this.pagedData.pageSize}`)
+   }
+
+   next(): IteratorResult<search.Result | null> {
+      // if we've reached the end of the current page, read the next page (overwriting current) and start from its beginning
+      if (this.index === this.currentData.length - 1) {
+         this.log.debug('loading next page')
+         this.currentPage = this.currentPage.next()
+         this.currentData = this.currentPage.data
+         this.index = 0
+      }
+
+      if (this.currentPage.isLast && this.currentData.length <= this.index) {
+         return {
+            done: true,
+            value: null
+         }
+      } else return {
+         done: false,
+         value: this.currentData[this.index++]
+      }
+   }
 
 
+}
 
 
 const log = LogManager.DefaultLogger;
 
+LogManager.getLogger(IterableSearch.LOGNAME).setLevel(LogManager.logLevel.debug)
 
-class IterableSearch<TResult> {
-
-   static loadSearch<TSearchResult> (id:string, pageSize?:number) {
-      return new IterableSearch<TSearchResult>(search.load({id: id }),pageSize)
-   }
-
-   pagedData : search.PagedData
-   currentPage : search.Page
-   currentData: search.Result[]
-   currentIndex: number
-
-   [Symbol.iterator](): Iterator<TResult> {
-      return this
-   }
-
-   private constructor(private search: search.Search, private pageSize? :number) {
-      this.pagedData = this.search.runPaged({pageSize: pageSize})
-      this.currentPage = this.pagedData.fetch({index:0})
-      this.currentData = this.currentPage.data
-      this.currentIndex = 0
-   }
-
-   next() : IteratorResult<TResult> {
-      // if we've reached the end of the current page, read the next page (overwriting current) and start from its beginning
-      if (this.currentIndex === this.currentData.length-1) {
-         this.currentPage = this.currentPage.next()
-         this.currentData = this.currentPage.data
-         this.currentIndex = 0
-      }
-
-      if (this.currentPage.isLast && this.currentData.length <= this.currentIndex) {
-         return {
-            done: true
-         }
-      } else return {
-         done:false,
-         value: this.currentData[this.currentIndex++]
-      }
-   }
-}
 
 namespace X {
-
-   function observableSearch(id) {
-         const pages = search.load({id: id}).runPaged()
-         pages.pageRanges
-            .forEach(pageRange => {
-               pages.fetch({index: pageRange.index}).data.forEach(result => {
-                  return nsSearchResult2obj(result)
-               })
-            })
-      }
 
    export function onRequest(ctx) {
       switch (ctx.request.method) {
          case "GET":
             log.debug('GET request')
+            const s = IterableSearch.loadSearch('730')
+
+            Seq(s)
+               .take(1)
+               .map(nsSearchResult2obj)
+               .forEach(i=> log.debug('result',i))
+
+            log.debug('first result', nsSearchResult2obj(s.next().value!))
+            log.debug('second result', nsSearchResult2obj(s.next().value!))
+            log.debug('third result', nsSearchResult2obj(s.next().value!))
             break
          case "POST":
             log.debug('POST request parms', ctx.request.parameters)
