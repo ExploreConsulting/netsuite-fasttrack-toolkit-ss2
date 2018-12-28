bbe67106de38cf21571aa03b55ffb59a70ecafa2
Shawn Talbert <Shawn.Talbert@rsmus.com>
2018-12-28 11:17:05 -0800

    add tests to demonstrate subtle issue with unintentional eager evaluation of Seq when logging

--- test/search.test.js
+++ test/search.test.js
@@ -4,12 +4,13 @@
         if (v !== undefined) module.exports = v;
     }
     else if (typeof define === "function" && define.amd) {
-        define(["require", "exports", "../search"], factory);
+        define(["require", "exports", "../search", "immutable"], factory);
     }
 })(function (require, exports) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     var search_1 = require("../search");
+    var immutable_1 = require("immutable");
     describe('nsSearchResult2obj', function () {
         /**
          *
@@ -85,6 +86,41 @@
                 fooLabelText: 'value text',
                 bar: 5
                 // note no 'barText' here
+            });
+        });
+        describe('ImuutableJS behavior', function () {
+            test('indirect toString() of Seq causes eager eval', function () {
+                var alwaysTrue = jest.fn(function (val) {
+                    console.log("alwaysTrue called with value " + val);
+                    return true;
+                });
+                immutable_1.Seq.of(1, 2, 3, 4, 5)
+                    .takeWhile(alwaysTrue)
+                    // subtle issue - this causes repeated eager evaluation due to serializing the 3rd argument passed
+                    .forEach(console.log);
+                // above forEach() passes value, key, and the *entire iterable* to console.log. console.log then proceeds to
+                // convert its given arguments to strings. When toString() is called on the third param (the iterable sequence)
+                // it forces eager evaluation of the whole sequence because a Seq() has toString() defined to do just that.
+                //
+                // So each value of the sequence is first passed through takeWhile() predicate (alwaysTrue()) then the
+                // entire sequence is passed through alwaysTrue() again as it's being serialized to [1,2,3,4,5]. This results
+                // in 6 invocations of alwaysTrue() for each element - once as expected by the forEach() and 5 more as we
+                // reserialize the entire sequence (1..5)
+                // see next test for workaround
+                expect(alwaysTrue).toBeCalledTimes(5 * 6);
+            });
+            test('how to avoid eager eval of Seq', function () {
+                var alwaysTrue = jest.fn(function (val) {
+                    console.log("alwaysTrue called with value " + val);
+                    return true;
+                });
+                immutable_1.Seq.of(1, 2, 3, 4, 5)
+                    .takeWhile(alwaysTrue)
+                    // arity-1 function will NOT cause repeated eager evaluation of the sequence 1..5
+                    // because console.log only proceses the value, not also receiving the key and iterable
+                    .forEach(function (x) { return console.log(x); });
+                // above forEach() passes just value to console.log
+                expect(alwaysTrue).toBeCalledTimes(5);
             });
         });
     });
