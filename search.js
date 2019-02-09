a034c97bd86d3d4db20d86fc651f37c4cdea426f
Shawn Talbert <Shawn.Talbert@rsmus.com>
2019-02-09 08:46:44 -0800

    improving search docs

--- search.js
+++ search.js
@@ -1,16 +1,27 @@
+/**
+ * This module provides a lazy, functional processing approach to working with NetSuite saved searches.
+ * It automatically handles paging behind the scenes allowing the developer to focus on 'per result' business logic.
+ *
+ * Use `LazySearch.from()` and `LazySearch.load()` to get started.
+ * Turn search results into plain objects using `nsSearchResult2obj()` and leverage
+ * the methods of [ImmutableJS](https://facebook.github.io/immutable-js/) to process search results.
+ */
 (function (factory) {
     if (typeof module === "object" && typeof module.exports === "object") {
         var v = factory(require, exports);
         if (v !== undefined) module.exports = v;
     }
     else if (typeof define === "function" && define.amd) {
-        define(["require", "exports", "./lodash", "N/search", "./EC_Logger", "./governance"], factory);
+        define(["require", "exports", "N/search", "./lodash", "./EC_Logger", "./governance"], factory);
     }
 })(function (require, exports) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
-    var _ = require("./lodash");
+    /**
+     * dummy comment for the import so TypeDoc renders the comment above this one
+     */
     var search = require("N/search");
+    var _ = require("./lodash");
     var LogManager = require("./EC_Logger");
     // include this as a convenience since it will often be used with arbitrary long search results to manage governance
     var governance_1 = require("./governance");
@@ -25,13 +36,20 @@
      * @returns a mapping function taking a NetSuite search result and returns a POJO representation of that search result.
      * The return type will always have an 'id' property merged with type T if provided.
      *
+     * @typeparam T declares the shape of the plain objects returned. e.g. `nsSearchResult2obj<{ companyname, memo }>` for a search result
+     * that has columns _companyname_ and _memo_. Including an optional type here ensures strong typing on followup chained
+     * method calls.
+     *
      * @example
      *
+     * ```typescript
      *  // default (uses column labels if present)
      *  Seq(LazySearch.load(1234)).map(nsSearchResult2obj()).forEach(...)
      *
      *  // force ignoring search column labels
      *  Seq(LazySearch.load(1234)).map(nsSearchResult2obj(false)).forEach(...)
+     *
+     *  ```
      */
     function nsSearchResult2obj(useLabels) {
         if (useLabels === void 0) { useLabels = true; }
@@ -52,7 +70,7 @@
     }
     exports.nsSearchResult2obj = nsSearchResult2obj;
     /**
-     * makes a NetSuite search an ES2015 style Iterator. That is, it follows the Iterator Protocol for iterating
+     * Makes a NetSuite search an ES2015 style Iterator. That is, it follows the Iterator Protocol for iterating
      * over search results in a forward-only fashion. The result can be passed to any library
      * that accepts Iterators (such as ImmutableJS)
      * to provide easy chainable logic on arbitrary length search result sets.
@@ -64,15 +82,17 @@
      * I've heard Ramda may support iterators so if we choose to go a more pure FP route down the
      * road this class would be useful - i.e. it remains untied to any particular library.
      *
-     * @example using immutablejs Sequence
-     * import {Seq} from 'immutable'
-     * const oneResult = Seq(LazySearch.load('1234')).map(nsSearchResult2obj).take(1)
+     * @example take the first result of a search as a plain object
+     * ```typescript
+     * import {Seq} from './NFT-X.Y.Z/immutable'
+     * const oneResult = Seq(LazySearch.load('1234')).map(nsSearchResult2obj()).take(1)
+     * ```
      */
     var LazySearch = /** @class */ (function () {
         /**
          * Not meant to be used directly, use factory methods such as `load` or `from`
-         * @param {Search} search
-         * @param {number} pageSize optional pagesize, can be up to 1000
+         * @param search the netsuite search object to wrap
+         * @param pageSize optional pagesize, can be up to 1000
          */
         function LazySearch(search, pageSize) {
             if (pageSize === void 0) { pageSize = 500; }
@@ -96,18 +116,48 @@
         }
         /**
          * Loads an existing NS search by id and prepares it for lazy evaluation
-         * @param {string} id
-         * @param {number} pageSize
+         * @param id internal id of the search to load
+         * @param pageSize how many records to retrieve per page (paging is automatic) Maximum value: 1000
          * @returns {LazySearch}
+         *
+         * @example do something for each search result, automatically exiting if out of governance.
+         *
+         * ```typescript
+         *
+         * import {Seq} from './NFT-X.Y.Z/immutable'
+         * import {governanceRemains, LazySearch, nsSearchResult2obj} from './NFT-X.Y.Z/search'
+         *
+         * Seq(LazySearch.load('1234'))
+         *   .takeWhile(governanceRemains()) // process until we drop below default governance threshold
+         *   .map(nsSearchResult2obj()) // convert search results to plain objects with properties
+         *   .forEach( r => log.debug(r))
+         * ```
          */
         LazySearch.load = function (id, pageSize) {
             return new LazySearch(search.load({ id: id }), pageSize);
         };
         /**
-         * creates a lazy search from an existing NS search
-         * @param {Search} search
-         * @param {number} pageSize
+         * Creates a lazy search from an existing NS search.
+         * @param search
+         * @param pageSize
          * @returns {LazySearch}
+         *
+         * @example create a search and begin lazy processing of results
+         *
+         * ```
+         * import {Seq} from './NFT-X.Y.Z/immutable'
+         * import * as search from 'N/search
+         * import {governanceRemains, LazySearch, nsSearchResult2obj} from './NFT-X.Y.Z/search'
+         *
+         * Seq(LazySearch.from(search.create({
+         *    filters: [['internalid', 'anyof', [1,2]),
+         *    columns:['item', 'description'],
+         *    type: search.Type.ITEM,
+         *  })))
+         *   .takeWhile(governanceRemains()) // process until we drop below default governance threshold
+         *   .map(nsSearchResult2obj()) // convert search results to plain objects with properties
+         *   .forEach( r => log.debug(r))
+         * ```
          */
         LazySearch.from = function (search, pageSize) {
             return new LazySearch(search, pageSize);
@@ -115,6 +165,8 @@
         /**
          * per the iterator protocol, retrieves the next element. Also returns `null` if done as the specification for
          * the protocol says the value property is optional when 'done'
+         *
+         * You don't typically call this function yourself - libraries like ImmutableJS do.
          * @returns {IteratorResult<Result | null>}
          */
         LazySearch.prototype.next = function () {
@@ -138,7 +190,9 @@
                 value: this.currentData[this.index++]
             };
         };
-        // the name of the custom logger for this component for independent logging control
+        /**
+         * the name of the custom logger for this component for independent logging control
+         */
         LazySearch.LOGNAME = 'lazy';
         return LazySearch;
     }());
