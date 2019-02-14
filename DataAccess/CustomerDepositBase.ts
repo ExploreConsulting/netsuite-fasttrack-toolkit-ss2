55a07d57d4a20abb5e86cada4c1eac5617038e16
Shawn Talbert <Shawn.Talbert@rsmus.com>
2019-02-14 08:06:06 -0800

    add rudimentary CustomerDepositBase

---
+++ DataAccess/CustomerDepositBase.ts
@@ -0,0 +1,34 @@
+/**
+ * Base Customer Deposit (customerdeposit) definition
+ */
+
+import { FieldType } from './Record'
+import { TransactionBase } from './Transaction'
+import * as record from 'N/record'
+
+/**
+ * NetSuite Customer Deposit Record 'customerdeposit'
+ */
+export class CustomerDepositBase extends TransactionBase {
+
+   static recordType = record.Type.CUSTOMER_DEPOSIT
+
+   @FieldType.select
+   account: number
+
+   @FieldType.select
+   currency: number
+
+   @FieldType.select
+   customer: number
+
+   @FieldType.select
+   paymentmethod: number
+
+   @FieldType.select
+   salesorder: number
+
+   @FieldType.currency
+   payment: string
+}
+
