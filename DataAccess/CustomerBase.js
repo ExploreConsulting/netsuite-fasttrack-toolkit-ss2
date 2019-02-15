5f9b95967ebe0927f5963371ab9d28a962798606
Shawn Talbert <Shawn.Talbert@rsmus.com>
2019-02-15 09:06:09 -0800

    adding rudimentary 'addressbook' sublist support and related subrecord to Customer definition. With this you can access a customer address subrecord via something like `customer.addressbook[0].addressbookaddress`

--- DataAccess/CustomerBase.js
+++ DataAccess/CustomerBase.js
@@ -34,6 +34,121 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     var Record_1 = require("./Record");
     var record = require("N/record");
     var Sublist_1 = require("./Sublist");
+    /**
+     * The addressbook 'subrecord'. In SS2.0 this is mostly treated as a normal record object.
+     * However I caution against trying to create new instances of it, only passing an existing record
+     * to the constructor. For example, on the customer record you can get an address sublist record
+     * with Record.getSublistSubrecord() then pass the returned record to the Address constructor.
+     * Currently this has only been tested for read access to aqddress properties defined below.
+     */
+    var AddressBase = /** @class */ (function (_super) {
+        __extends(AddressBase, _super);
+        function AddressBase() {
+            return _super !== null && _super.apply(this, arguments) || this;
+        }
+        __decorate([
+            Record_1.FieldType.freeformtext
+        ], AddressBase.prototype, "addr1", void 0);
+        __decorate([
+            Record_1.FieldType.freeformtext
+        ], AddressBase.prototype, "addr2", void 0);
+        __decorate([
+            Record_1.FieldType.freeformtext
+        ], AddressBase.prototype, "addr3", void 0);
+        __decorate([
+            Record_1.FieldType.freeformtext
+        ], AddressBase.prototype, "addressee", void 0);
+        __decorate([
+            Record_1.FieldType.freeformtext
+        ], AddressBase.prototype, "addrphone", void 0);
+        __decorate([
+            Record_1.FieldType.freeformtext
+        ], AddressBase.prototype, "addrtext", void 0);
+        __decorate([
+            Record_1.FieldType.freeformtext
+        ], AddressBase.prototype, "attention", void 0);
+        __decorate([
+            Record_1.FieldType.freeformtext
+        ], AddressBase.prototype, "city", void 0);
+        __decorate([
+            Record_1.FieldType.select
+        ], AddressBase.prototype, "country", void 0);
+        __decorate([
+            Record_1.FieldType.freeformtext
+        ], AddressBase.prototype, "state", void 0);
+        __decorate([
+            Record_1.FieldType.freeformtext
+        ], AddressBase.prototype, "zip", void 0);
+        __decorate([
+            Record_1.FieldType.checkbox
+        ], AddressBase.prototype, "override", void 0);
+        return AddressBase;
+    }(Record_1.NetsuiteRecord));
+    exports.AddressBase = AddressBase;
+    /**
+     * The address _sublist_ on customer records, not to be confused with the Address _subrecord_.
+     * Customer address info is split between this sublist and the subrecord pointed to by the _addressbook_ field.
+     */
+    var AddressSublist = /** @class */ (function (_super) {
+        __extends(AddressSublist, _super);
+        function AddressSublist() {
+            return _super !== null && _super.apply(this, arguments) || this;
+        }
+        Object.defineProperty(AddressSublist.prototype, "addressbookaddress", {
+            /**
+             * Address subrecord
+             */
+            get: function () {
+                return new AddressBase(this.nsrecord.getSublistSubrecord({
+                    sublistId: 'addressbook',
+                    fieldId: 'addressbookaddress',
+                    line: this._line
+                }));
+            },
+            enumerable: true,
+            configurable: true
+        });
+        __decorate([
+            Sublist_1.SublistFieldType.freeformtext
+        ], AddressSublist.prototype, "attention", void 0);
+        __decorate([
+            Sublist_1.SublistFieldType.checkbox
+        ], AddressSublist.prototype, "defaultbilling", void 0);
+        __decorate([
+            Sublist_1.SublistFieldType.checkbox
+        ], AddressSublist.prototype, "defaultshipping", void 0);
+        __decorate([
+            Sublist_1.SublistFieldType.freeformtext
+        ], AddressSublist.prototype, "displaystate", void 0);
+        __decorate([
+            Sublist_1.SublistFieldType.select
+        ], AddressSublist.prototype, "dropdownstate", void 0);
+        __decorate([
+            Sublist_1.SublistFieldType.integernumber
+        ], AddressSublist.prototype, "id", void 0);
+        __decorate([
+            Sublist_1.SublistFieldType.integernumber
+        ], AddressSublist.prototype, "internalid", void 0);
+        __decorate([
+            Sublist_1.SublistFieldType.checkbox
+        ], AddressSublist.prototype, "isresidential", void 0);
+        __decorate([
+            Sublist_1.SublistFieldType.freeformtext
+        ], AddressSublist.prototype, "label", void 0);
+        __decorate([
+            Sublist_1.SublistFieldType.checkbox
+        ], AddressSublist.prototype, "override", void 0);
+        __decorate([
+            Sublist_1.SublistFieldType.freeformtext
+        ], AddressSublist.prototype, "phone", void 0);
+        __decorate([
+            Sublist_1.SublistFieldType.freeformtext
+        ], AddressSublist.prototype, "state", void 0);
+        __decorate([
+            Sublist_1.SublistFieldType.freeformtext
+        ], AddressSublist.prototype, "zip", void 0);
+        return AddressSublist;
+    }(Sublist_1.SublistLine));
     var CustomerBase = /** @class */ (function (_super) {
         __extends(CustomerBase, _super);
         function CustomerBase() {
@@ -115,60 +230,12 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
         __decorate([
             Record_1.FieldType.select
         ], CustomerBase.prototype, "terms", void 0);
+        __decorate([
+            Record_1.FieldType.sublist(AddressSublist)
+        ], CustomerBase.prototype, "addressbook", void 0);
         return CustomerBase;
     }(Record_1.NetsuiteRecord));
     exports.CustomerBase = CustomerBase;
-    /**
-     * The addressbook 'subrecord'. In SS2.0 this is mostly treated as a normal record object.
-     * However I caution against trying to create new instances of it, only passing an existing record
-     * to the constructor. For example, on the customer record you can get an address sublist record
-     * with Record.getSublistSubrecord() then pass the returned record to the Address constructor.
-     * Currently this has only been tested for read access to aqddress properties defined below.
-     */
-    var AddressBase = /** @class */ (function (_super) {
-        __extends(AddressBase, _super);
-        function AddressBase() {
-            return _super !== null && _super.apply(this, arguments) || this;
-        }
-        __decorate([
-            Record_1.FieldType.freeformtext
-        ], AddressBase.prototype, "addr1", void 0);
-        __decorate([
-            Record_1.FieldType.freeformtext
-        ], AddressBase.prototype, "addr2", void 0);
-        __decorate([
-            Record_1.FieldType.freeformtext
-        ], AddressBase.prototype, "addr3", void 0);
-        __decorate([
-            Record_1.FieldType.freeformtext
-        ], AddressBase.prototype, "addressee", void 0);
-        __decorate([
-            Record_1.FieldType.freeformtext
-        ], AddressBase.prototype, "addrphone", void 0);
-        __decorate([
-            Record_1.FieldType.freeformtext
-        ], AddressBase.prototype, "addrtext", void 0);
-        __decorate([
-            Record_1.FieldType.freeformtext
-        ], AddressBase.prototype, "attention", void 0);
-        __decorate([
-            Record_1.FieldType.freeformtext
-        ], AddressBase.prototype, "city", void 0);
-        __decorate([
-            Record_1.FieldType.select
-        ], AddressBase.prototype, "country", void 0);
-        __decorate([
-            Record_1.FieldType.freeformtext
-        ], AddressBase.prototype, "state", void 0);
-        __decorate([
-            Record_1.FieldType.freeformtext
-        ], AddressBase.prototype, "zip", void 0);
-        __decorate([
-            Record_1.FieldType.checkbox
-        ], AddressBase.prototype, "override", void 0);
-        return AddressBase;
-    }(Record_1.NetsuiteRecord));
-    exports.AddressBase = AddressBase;
     var ContactsSublist = /** @class */ (function (_super) {
         __extends(ContactsSublist, _super);
         function ContactsSublist() {
--- DataAccess/CustomerBase.ts
+++ DataAccess/CustomerBase.ts
@@ -4,7 +4,115 @@
 
 import { FieldType, NetsuiteRecord } from './Record'
 import * as record from 'N/record'
-import { SublistFieldType, SublistLine } from './Sublist'
+import { Sublist, SublistFieldType, SublistLine } from './Sublist'
+
+/**
+ * The addressbook 'subrecord'. In SS2.0 this is mostly treated as a normal record object.
+ * However I caution against trying to create new instances of it, only passing an existing record
+ * to the constructor. For example, on the customer record you can get an address sublist record
+ * with Record.getSublistSubrecord() then pass the returned record to the Address constructor.
+ * Currently this has only been tested for read access to aqddress properties defined below.
+ */
+export class AddressBase extends NetsuiteRecord {
+
+   @FieldType.freeformtext
+   addr1:string
+
+   @FieldType.freeformtext
+   addr2:string
+
+   @FieldType.freeformtext
+   addr3:string
+
+   @FieldType.freeformtext
+   addressee:string
+
+   /**
+    * note this field name differs from the 'records browser' documentation
+    */
+   @FieldType.freeformtext
+   addrphone:string
+
+   @FieldType.freeformtext
+   addrtext:string
+
+   @FieldType.freeformtext
+   attention:string
+
+   @FieldType.freeformtext
+   city:string
+
+   @FieldType.select
+   country:number
+
+   @FieldType.freeformtext
+   state:string
+
+   @FieldType.freeformtext
+   zip:string
+
+   @FieldType.checkbox
+   override:boolean
+}
+
+
+/**
+ * The address _sublist_ on customer records, not to be confused with the Address _subrecord_.
+ * Customer address info is split between this sublist and the subrecord pointed to by the _addressbook_ field.
+ */
+class AddressSublist extends SublistLine {
+
+   /**
+    * Address subrecord
+    */
+   get addressbookaddress() : AddressBase {
+      return new AddressBase(this.nsrecord.getSublistSubrecord({
+         sublistId:'addressbook',
+         fieldId:'addressbookaddress',
+         line: this._line
+      }))
+   }
+
+   @SublistFieldType.freeformtext
+   attention:string
+
+   @SublistFieldType.checkbox
+   defaultbilling:boolean
+
+   @SublistFieldType.checkbox
+   defaultshipping:boolean
+
+   @SublistFieldType.freeformtext
+   displaystate:string
+
+   @SublistFieldType.select
+   dropdownstate:number
+
+   @SublistFieldType.integernumber
+   id: number
+
+   @SublistFieldType.integernumber
+   internalid:number
+
+   @SublistFieldType.checkbox
+   isresidential: boolean
+
+   @SublistFieldType.freeformtext
+   label: string
+
+   @SublistFieldType.checkbox
+   override:boolean
+
+   @SublistFieldType.freeformtext
+   phone:string
+
+   @SublistFieldType.freeformtext
+   state:string
+
+   @SublistFieldType.freeformtext
+   zip:string
+}
+
 
 export class CustomerBase extends NetsuiteRecord {
    static recordType = record.Type.CUSTOMER
@@ -83,56 +191,11 @@ export class CustomerBase extends NetsuiteRecord {
 
    @FieldType.select
    terms:number
+
+   @FieldType.sublist(AddressSublist)
+   addressbook: Sublist<AddressSublist>
 }
 
-/**
- * The addressbook 'subrecord'. In SS2.0 this is mostly treated as a normal record object.
- * However I caution against trying to create new instances of it, only passing an existing record
- * to the constructor. For example, on the customer record you can get an address sublist record
- * with Record.getSublistSubrecord() then pass the returned record to the Address constructor.
- * Currently this has only been tested for read access to aqddress properties defined below.
- */
-export class AddressBase extends NetsuiteRecord {
-
-   @FieldType.freeformtext
-   addr1:string
-
-   @FieldType.freeformtext
-   addr2:string
-
-   @FieldType.freeformtext
-   addr3:string
-
-   @FieldType.freeformtext
-   addressee:string
-
-   /**
-    * note this field name differs from the 'records browser' documentation
-    */
-   @FieldType.freeformtext
-   addrphone:string
-
-   @FieldType.freeformtext
-   addrtext:string
-
-   @FieldType.freeformtext
-   attention:string
-
-   @FieldType.freeformtext
-   city:string
-
-   @FieldType.select
-   country:number
-
-   @FieldType.freeformtext
-   state:string
-
-   @FieldType.freeformtext
-   zip:string
-
-   @FieldType.checkbox
-   override:boolean
-}
 
 export class ContactsSublist extends SublistLine {
 
