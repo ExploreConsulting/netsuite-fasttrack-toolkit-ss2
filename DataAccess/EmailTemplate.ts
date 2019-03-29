5cb736312f67ab5982102761f2dec0766ee27853
Shawn Talbert <Shawn.Talbert@rsmus.com>
2019-03-29 07:56:19 -0700

    add EmailTemplate from JohnE

---
+++ DataAccess/EmailTemplate.ts
@@ -0,0 +1,47 @@
+/**
+ * NetSuite Email Template Record Type
+ */
+import {FieldType, NetsuiteRecord} from './Record'
+
+/**
+ * NetSuite Email Template Record Type (emailtemplate)
+ */
+export class EmailTemplate extends NetsuiteRecord {
+   static recordType = 'emailtemplate'
+
+   @FieldType.checkbox
+   addcompanyaddress: boolean
+
+   @FieldType.checkbox
+   addunsubscribelink: boolean
+
+   @FieldType.select
+   campaigndomain: number
+
+   @FieldType.textarea
+   content: string
+
+   @FieldType.textarea
+   description: string
+
+   @FieldType.freeformtext
+   externalid: string
+
+   @FieldType.checkbox
+   isinactive: boolean
+
+   @FieldType.checkbox
+   isprivate: boolean
+
+   @FieldType.select
+   mediaitem: number
+
+   @FieldType.freeformtext
+   name: string
+
+   @FieldType.select
+   restricttogroup: number
+
+   @FieldType.freeformtext
+   subject: string
+}
