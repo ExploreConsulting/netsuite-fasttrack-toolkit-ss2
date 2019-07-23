48355426f5d882e1e9b944f799b19c8912b4dd45
Shawn Talbert <Shawn.Talbert@rsmus.com>
2019-07-22 18:43:09 -0700

    experiment using rxjs observables for basic node callbacks

--- CodeGeneration/generate.ts
+++ CodeGeneration/generate.ts
@@ -1,21 +1,40 @@
 #!/usr/bin/env node
 
-import {execSync} from 'child_process'
+import { execSync } from 'child_process'
+import { promisify } from 'util'
 import * as commander from 'commander'
+import * as fs from 'fs'
+import { bindNodeCallback,of } from 'rxjs'
+import {catchError, map, first } from 'rxjs/operators'
+import { Stats } from 'fs'
+
+const stat = bindNodeCallback(fs.stat)
+
 
 
 const program = new commander.Command()
 program.version(require('./package.json').version)
-program.option('-f, --foo','does some foo')
-program.option('-d, --debug','output debug stuffs')
-program.command('subcommand')
-   .action( (e,o)=>console.log('subcommand'))
+program.option('-f, --foo', 'does some foo')
+program.option('-d, --debug', 'output debug stuffs')
+program.command('isproject')
+   .action((e, o) => {
+      isSDFproject().subscribe(v => {
+         console.log(`is SDF project? ${v}`)
+      })
+      console.log('after isdf prject')
+   })
+
 program.parse(process.argv)
 if (program.debug) console.log(program.opts())
 
+function isSDFproject () {
+     // return an empty Stats rather than throwing an exception
+   return stat('FileCabinet').pipe(
+        catchError(e=> of(new Stats())),
+        map( x=> !!x.ino), first())
+}
 
-
-const result = execSync("echo 'hello world'", { stdio:'inherit'})
+//const result = execSync('echo \'hello world\'', { stdio: 'inherit' })
 
 //TODO: feature - bootstrap authentication? resuse existing SDF config? expect users to have TBA already setup? use existing .SDF?
 
@@ -28,16 +47,6 @@ const result = execSync("echo 'hello world'", { stdio:'inherit'})
 //TODO: feature - generate code for transaction body custom fields
 //TODO: feature - generate code for transaction column custom fields
 
-
-
-
-
-
-
-
-
-
-
-console.log('hello')
+// console.log('hello')
 
 
