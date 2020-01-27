03db69c1839670b68372163eedc4019a81f49521
Shawn Talbert <Shawn.Talbert@rsmus.com>
2020-01-27 10:40:10 -0800

    update to latest jest/typescript and related test/doc libs.

--- __mocks__/N/runtime.js
+++ __mocks__/N/runtime.js
@@ -1,5 +1,15 @@
-"use strict";
-exports.__esModule = true;
-exports.getCurrentScript = jest.fn().mockName('getCurrentScript').mockReturnValue({
-    getRemainingUsage: jest.fn().mockName('getRemainingUsage')
+(function (factory) {
+    if (typeof module === "object" && typeof module.exports === "object") {
+        var v = factory(require, exports);
+        if (v !== undefined) module.exports = v;
+    }
+    else if (typeof define === "function" && define.amd) {
+        define(["require", "exports"], factory);
+    }
+})(function (require, exports) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+    exports.getCurrentScript = jest.fn().mockName('getCurrentScript').mockReturnValue({
+        getRemainingUsage: jest.fn().mockName('getRemainingUsage')
+    });
 });
