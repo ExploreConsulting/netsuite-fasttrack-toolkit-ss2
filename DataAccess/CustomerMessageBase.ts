7261ab2380c2f620b23e04986d1de93e1b4e3a55
Shawn Talbert <Shawn.Talbert@rsmus.com>
2020-03-31 12:08:13 -0700

    Add Customer Message base record type

---
+++ DataAccess/CustomerMessageBase.ts
@@ -0,0 +1,29 @@
+/**
+ * Represents an Customer Message (customermessage) record type in NetSuite
+ */
+
+import { FieldType, NetsuiteRecord } from './Record'
+import * as record from 'N/record'
+
+/**
+ * Customer Message (customermessage) Base Type
+ */
+export class CustomerMessageBase extends NetsuiteRecord {
+
+   static recordType = record.Type.CUSTOMER_MESSAGE
+
+   @FieldType.textarea
+   description: string
+
+   @FieldType.freeformtext
+   externalid: string
+
+   @FieldType.checkbox
+   isinactive: boolean
+
+   @FieldType.freeformtext
+   name: string
+
+   @FieldType.checkbox
+   preferred: boolean
+}
