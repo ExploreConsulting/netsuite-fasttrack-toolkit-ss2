c1b983420c5d8b38ea594ad0e9c4133f117fb069
stalbert <Shawn.Talbert@rsmus.com>
2021-08-12 08:20:52 -0700

    Add additional fields documented for Purchase Orders

--- DataAccess/PurchaseOrderBase.ts
+++ DataAccess/PurchaseOrderBase.ts
@@ -5,7 +5,7 @@
 import * as record from 'N/record'
 import { TransactionBase } from './Transaction'
 import { Sublist, SublistFieldType, SublistLine } from './Sublist'
-import { FieldType } from './Record'
+import { FieldType, Nullable } from './Record'
 import { AddressBase } from './AddressBase'
 
 /**
@@ -14,42 +14,95 @@ import { AddressBase } from './AddressBase'
 export class ItemSublist extends SublistLine {
 
    @SublistFieldType.decimalnumber
-   amount:number
+   amount: number
 
    @SublistFieldType.select
-   class:number
+   class: number
 
    @SublistFieldType.select
-   customer:number
+   customer: number
 
    @SublistFieldType.checkbox
-   deferrevrec:boolean
+   deferrevrec: boolean
 
    @SublistFieldType.select
-   department:number
+   department: number
 
    @SublistFieldType.textarea
-   description:string
+   description: string
+
+   @SublistFieldType.date
+   expectedreceiptdate: Date
 
    @SublistFieldType.checkbox
    isclosed: boolean
 
    @SublistFieldType.select
-   item:number
+   item: number
+
+   @SublistFieldType.integernumber
+   leadtime: Nullable<number>
+
+   @SublistFieldType.float
+   quantity: number
+
+   @SublistFieldType.float
+   quantityreceived: number
+
+   @SublistFieldType.float
+   quantitybilled: number
 
    @SublistFieldType.decimalnumber
-   quantity:number
+   rate: number
 
-   @SublistFieldType.decimalnumber
-   rate:number
+   @SublistFieldType.select
+   units: number
 
+   // this is documented as `text` in the records browser but shows up as what appears to be vendor internal id on records.
+   @SublistFieldType.freeformtext
+   vendorname: string | number
 }
 
 /**
  * NetSuite Purchase Order Record
  */
 export class PurchaseOrderBase extends TransactionBase {
-   static recordType() { return record.Type.PURCHASE_ORDER }
+
+   @FieldType.select
+   approvalstatus: number
+
+   @FieldType.currency
+   balance: Nullable<number>
+
+   @FieldType.select
+   class: Nullable<number>
+
+   @FieldType.select
+   createdfrom: Nullable<number>
+
+   @FieldType.select
+   currency: number
+
+   @FieldType.select
+   employee: Nullable<number>
+
+   @FieldType.select
+   incoterm: Nullable<number>
+
+   @FieldType.select
+   intercotransaction: Nullable<number>
+
+   @FieldType.checkbox
+   isbasecurrency: boolean
+
+   @FieldType.date
+   shipdate: Date
+
+   @FieldType.select
+   shipmethod: Nullable<number>
+
+   @FieldType.select
+   shipto: Nullable<number>
 
    @FieldType.subrecord(AddressBase)
    billingaddress: AddressBase
@@ -58,10 +111,27 @@ export class PurchaseOrderBase extends TransactionBase {
    shippingaddress: AddressBase
 
    @FieldType.select
-   approvalstatus:number
+   terms: Nullable<number>
+
+   @FieldType.checkbox
+   tobeemailed: Nullable<boolean>
+
+   @FieldType.checkbox
+   tobefaxed: Nullable<boolean>
+
+   @FieldType.checkbox
+   tobeprinted: Nullable<boolean>
+
+   @FieldType.currency
+   total: number
+
+   @FieldType.currency
+   unbilledorders: Nullable<number>
 
    @FieldType.sublist(ItemSublist)
    item: Sublist<ItemSublist>
+
+   static recordType () { return record.Type.PURCHASE_ORDER }
 }
 
 
