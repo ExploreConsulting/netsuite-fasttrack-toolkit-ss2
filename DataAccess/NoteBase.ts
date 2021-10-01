30b89ed58816b2e3cd7cc0ca1e18c82c1a9e9b58
stalbert <Shawn.Talbert@rsmus.com>
2021-10-01 10:59:23 -0700

    Add `NoteBase` originally from Hector.

---
+++ DataAccess/NoteBase.ts
@@ -0,0 +1,124 @@
+/**
+ * Base class for Note record
+ */
+import { FieldType, NetsuiteRecord } from './Record'
+
+/**
+ * Note record base class
+ */
+export class NoteBase extends NetsuiteRecord {
+   /**
+    * Record type
+    */
+   static recordType() {
+      return 'note'
+   }
+
+   /**
+    * Accounting Period
+    */
+   @FieldType.select
+   accountingperiod: number
+
+   /**
+    * Author
+    */
+   @FieldType.select
+   author: number
+
+   /**
+    * Direction
+    */
+   @FieldType.select
+   direction: number
+
+   /**
+    * Entity
+    */
+   @FieldType.select
+   entity: number
+
+   /**
+    * ExternalId
+    */
+   @FieldType.freeformtext
+   externalid: string
+
+   /**
+    * Folder
+    */
+   @FieldType.select
+   folder: number
+
+   /**
+    * Item
+    */
+   @FieldType.select
+   item: number
+
+   /**
+    * Modification Date
+    */
+   @FieldType.datetime
+   lastmodifieddate: Date
+
+   /**
+    * Media
+    */
+   @FieldType.select
+   media: number
+
+   /**
+    * Memo
+    */
+   @FieldType.textarea
+   note: string
+
+   /**
+    * Date
+    */
+   @FieldType.date
+   notedate: Date
+
+   /**
+    * Type
+    */
+   @FieldType.select
+   notetype: number
+
+   /**
+    * Custom Record Key
+    */
+   @FieldType.select
+   record: number
+
+   /**
+    * Custom Record Type
+    */
+   @FieldType.select
+   recordtype: number
+
+   /**
+    * Time
+    */
+   @FieldType.freeformtext
+   time: string
+
+   /**
+    * Title
+    */
+   @FieldType.freeformtext
+   title: string
+
+   /**
+    * Topic
+    */
+   @FieldType.select
+   topic: number
+
+   /**
+    * Transaction
+    */
+   @FieldType.select
+   transaction: number
+}
